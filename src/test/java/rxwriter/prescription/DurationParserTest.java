46504e51ad28dc09fcb0bbc5cc01eb795579a633
piotrk32 <piotrkepistyuwm@gmail.com>
2024-06-26 13:30:09 +0200

    initial commit for testing

---
+++ src/test/java/rxwriter/prescription/DurationParserTest.java
@@ -0,0 +1,19 @@
+package rxwriter.prescription;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DurationParserTest {
+
+    @Test
+    public void parseDurationWithValidUnitAndQuantity(){
+        soSomething();
+
+    }
+
+    private void soSomething(){
+        System.out.println("hi");
+    }
+
+}
